e604e8aabfcb00b70a63b63fe97f9338166b9ce8
BenjSta <61200235+BenjSta@users.noreply.github.com>
2025-02-02 12:20:13 +0100

    changes package version (as there might be minor changes, will finally be changed to 1.0.0)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="distillmos",
-    version="1.0.0",
+    version="0.9.0",
     description="Efficient speech quality assessment learned from SSL-based speech quality assessment model",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
